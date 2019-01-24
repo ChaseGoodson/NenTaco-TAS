18a0e6413d1cae4cd406d364189402521b34682d
Eric Potter <eric.b.potter@gmail.com>
2019-01-23 21:18:15 -0500

    Initial Classes

---
+++ NesBot/ButtonState.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NesBot
+{
+    public class ButtonState
+    {
+        public bool ButtonLeftIsPressed { get; set; }
+        public bool ButtonRightIsPressed { get; set; }
+        public bool ButtonUpIsPressed { get; set; }
+        public bool ButtonDownIsPressed { get; set; }
+        public bool ButtonAIsPressed { get; set; }
+        public bool ButtonBIsPressed { get; set; }
+
+        public static ButtonState MovingRight =>
+            new ButtonState() { ButtonRightIsPressed = true };
+        public static ButtonState Jumping =>
+            new ButtonState() { ButtonAIsPressed = true };
+        public static ButtonState MovingRightAndJumping =>
+            new ButtonState() { ButtonRightIsPressed = true,
+                                ButtonAIsPressed = true};
+    }
+}
--- NesBot/Program.cs
+++ NesBot/Program.cs
@@ -10,6 +10,20 @@ namespace NesBot
     {
         static void Main(string[] args)
         {
+            ControlSequence mySequnce = new ControlSequence();
+
+            ButtonState buttonStateOne = new ButtonState();
+            buttonStateOne.ButtonRightIsPressed = true;
+            mySequnce.Add(buttonStateOne);
+
+            ButtonState buttonStateTwo = new ButtonState();
+            buttonStateTwo.ButtonUpIsPressed = true;
+            mySequnce.Add(buttonStateTwo);
+
+            // TODO - print the button sequence out to the console.
+
+            Console.WriteLine("Press Any Key To Continue");
+            Console.ReadKey();
         }
     }
 }
