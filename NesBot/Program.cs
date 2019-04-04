f0bf9a81b5c976716d046d037f47ad052f4c9eb6
Goodson, Chase E - 01 <CEGoodson01@indianatech.net>
2019-04-03 20:59:20 -0400

    added achievement text file

--- NesBot/Program.cs
+++ NesBot/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NesBot.Achievements;
+using System.IO;
+
 
 
 namespace NesBot
@@ -16,31 +18,45 @@ namespace NesBot
             ControlSequence mySequence = new ControlSequence();
 
             ControlSequence run = new ControlSequence();
-            run = run.RunTwo();
+            run = run.RunOne();
             
             var listOfRuns = new List<ControlSequence>();
             listOfRuns.Add(run);
-
-            //SequencePrinter printer = new SequencePrinter();
-			//printer.Print(run);
-
+            
             var player = new GamePlayer();
             var results = player.Play(listOfRuns);
 
             var eval = new DistanceAchievementEvaluator();
             var achievement = eval.GetAchievements(results);
-            foreach (var a in achievement)
-            {
-                Console.WriteLine(a);
-            }
 
-            
             Console.WriteLine(results.Max(r => r.MaximumHorizontalDistance));
-
-            var stepResults = player.Play(listOfRuns);
             Console.WriteLine(results.Max(r => r.StepWhereRunEnded));
 
-            
+            Console.WriteLine("Do you want the achievements to print to screen or to text file?");
+            Console.WriteLine("1) Screen");
+            Console.WriteLine("2) Text");
+            Console.WriteLine("Press 1 or 2");
+            int userPick = Convert.ToInt32(Console.ReadLine());
+            if (userPick == 1)
+            {
+                foreach (var a in achievement)
+                {
+                    Console.WriteLine(a);
+                }
+            }
+            else if (userPick == 2)
+            {
+                using (System.IO.StreamWriter file =
+                new System.IO.StreamWriter(@"C:\Users\cheez\Desktop\Achievements.txt"))
+                    foreach (var a in achievement)
+                    {
+                        file.WriteLine(a);
+                    }
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice. Your achievements are deleted.");
+            }
 
             Console.WriteLine("Press Any Key To Continue");
             Console.ReadKey();
